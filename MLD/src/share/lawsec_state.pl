00ba7315202132706dfcaeec69669492fd105d2e
Mateusz Lewicki <mateusz.lewicki@windowslive.com>
2020-03-09 09:54:58 +0000

    Update lawsec_state.pl

--- MLD/src/share/lawsec_state.pl
+++ MLD/src/share/lawsec_state.pl
@@ -10,6 +10,7 @@ use warnings;
 #my $LAWDIR=$ENV{'LAWDIR'};
 #my $secLog="$LAWDIR/system/sec.log";
 my $secLog = $ARGV[1]
+my $pl = $ARGV[2]
 my @mails=qw(mateusz.lewicki@atos.net);
 my $mail_list=join(" ", @mails);
 
@@ -17,7 +18,7 @@ while(1){
 
   unless (-e $secLog ) {
     my $lastSL="Lawson Security: Disabled";
-    `echo "PL: $ENV{'XXPDL'}\n\n$lastSL\n" | mailx -s "Lawsec" $mail_list` if ($lastSL =~ /Disabled/);}
+    `echo "PL: $pl\n\n$lastSL\n" | mailx -s "Lawsec" $mail_list` if ($lastSL =~ /Disabled/);}
   else{
 
     my $DOW=`date +%a`;
@@ -28,12 +29,12 @@ while(1){
 
     if ($lastSL[0] =~ /$DOW\s$MONTH\s\d{2}\s\d{2}:\d{2}:\d{2}\s$YEAR/){
 
-      if ($lastSL[4] =~ /Disabled/){
+      if ($lastSL[4] =~ /Enabled/){ # TMP  return to disabled
 
         my $out = join("\n",@lastSL);
 
-        `echo "PL: $ENV{'XXPDL'}\n\n$out\n" | mailx -s "Lawsec" $mail_list` unless ($ENV{'DBG_SEND_MAILS'} =~ /N|n/);
-        `echo "PL: $ENV{'XXPDL'}\n\n$out\n"` if ($ENV{'DBG_EXPLICIT'} =~ /Y|y/);
+        `echo "PL: $pl\n\n$out\n" | mailx -s "Lawsec" $mail_list` unless ($ENV{'DBG_SEND_MAILS'} =~ /N|n/);
+        `echo "PL: $pl\n\n$out\n"` if ($ENV{'DBG_EXPLICIT'} =~ /Y|y/);
       }
     }
   }
